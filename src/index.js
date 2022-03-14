44ca85f5c6e7e3d42193dae03305781ba2420245
srusanovskii <srusanovskii@inno.tech>
2022-03-14 18:17:09 +0300

    First COMMIT

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import styled, {createGlobalStyle} from "styled-components";
+
+const Global = createGlobalStyle`
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      font-family: "Courier New";
+    }
+`
+
+ReactDOM.render(
+  <>
+      <Global/>
+    <App/>
+  </>,
+  document.getElementById('root')
+);
+
+
